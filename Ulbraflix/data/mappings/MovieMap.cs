e9bcb3c93ff8a139e5b319a8827189d76f07347f
MATEUS SCHEFFERm <mateusscheffer13@gmail.com>
2022-07-01 20:59:13 -0300

    [CreateDataContext-6]Adding some maps

---
+++ Ulbraflix/data/mappings/MovieMap.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ulbraflix.entities;
+
+namespace Ulbraflix.data.mappings;
+
+public class MovieMap : IEntityTypeConfiguration<Movie>
+{
+    public void Configure(EntityTypeBuilder<Movie> builder)
+    {
+        builder.ToTable("movie");
+
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Duration)
+            .HasColumnName("duration");
+
+        builder.Property(e => e.LastMinuteWatched)
+            .HasColumnName("last_minute_watched");
+
+        builder.Property(e => e.Name)
+            .HasColumnName("name")
+            .HasMaxLength(100);
+        
+        builder.Property(e => e.Sinopsis)
+            .HasColumnName("sinopsis")
+            .HasMaxLength(256);
+
+        builder.Property(e => e.IsWatched)
+            .HasColumnName("is_watched")
+            .HasColumnType("SMALLINT");
+    }
+}
---
+++ Ulbraflix/data/mappings/RatingMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ulbraflix.entities;
+
+namespace Ulbraflix.data.mappings;
+
+public class RatingMap : IEntityTypeConfiguration<Rating>
+{
+    public void Configure(EntityTypeBuilder<Rating> builder)
+    {
+        builder.ToTable("rating");
+
+        builder.Property(e => e.Id)
+            .HasColumnName("id");
+
+        builder.Property(e => e.RatingValue)
+            .HasColumnName("rating_value");
+    }
+}
